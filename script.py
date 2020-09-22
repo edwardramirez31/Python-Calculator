08914ff2b7cb6b455fba54e5a617961e155d5045
Edward Ramirez <edal_ramirez@hotmail.com>
2020-09-22 14:08:39 -0500

    Some changes in the Calculator Class and Added a program controller (Controller class)
    The first change was adding the 'Times'Font to the display and setting the text alignment to the right.
    Another change was in the _createButtons dictionary, first I made an empty dict and added the button text as the key and the final button widget
    as the value in that dictionary, so I can have access to the widgets that I recently made.
    
    I included a big change. I made a Controller Class responsible of connecting the signals with another functions. If the button pressed is "CE", I
    connect the signal with the clearDisplay function, if the button pressed by the user is not "CE" or "=", the signal connects with the 'BuildingText'
    Function, this function receives the text button, gets the current display text and concatenate those strings. Then, calls the setDisplayText
    function, so the concatenated text is displayed on screen.
    
    PD: The controller class receives the Calculator window instance to get access to the dictionary that store all the button widgets.

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from functools import partial
 import sys
 
 
@@ -26,6 +27,8 @@ class Calculator(QMainWindow):
         self.display = QLineEdit()
         self.display.setFixedHeight(35)
         self.display.setReadOnly(True)
+        self.display.setFont(QFont('Times', 15))
+        self.display.setAlignment(Qt.AlignRight)
         self.mainLayout.addWidget(self.display)
         self.display.setStyleSheet("""
             background-color : #DAE0E2;
@@ -33,6 +36,7 @@ class Calculator(QMainWindow):
             border : 1px solid white""")
 
     def _createButtons(self):
+        self.buttons = {}
         self.buttonslayout = QGridLayout()
         """ Buttons dictionary where the key is the button text and the value is
         its grid location"""
@@ -67,22 +71,56 @@ class Calculator(QMainWindow):
             self.button.setStyleSheet("""QPushButton {
                                         background-color : #2C3335;
                                         color : white;
-                                        border-radius : 5px;
-                                        border : 1px solid white
+
                                       }
                                       QPushButton:hover {
                                         background-color: #7B8788;
                                       }
                                       """)
+            self.buttons[button_text] = self.button
             # add widget
-            self.buttonslayout.addWidget(self.button, location[0], location[1])
+            self.buttonslayout.addWidget(self.buttons[button_text], location[0], location[1])
         self.mainLayout.addLayout(self.buttonslayout)
+
+    # Calculator Functionality
+    def setDisplayText(self, text):
+        """This method sets and updates the display Text"""
+        self.display.setText(text)
+        self.display.setFocus()
+
+    def getDisplayText(self):
+        """This method get the text shown in the display when the user
+        clicks on '=' Button"""
+        return self.display.text()
+
+    def clearDisplay(self):
+        """This method clears the display when somebody presses the 'C' Button"""
+        self.display.setText('')
+
+
+class Controller:
+    def __init__(self, window):
+        self._window = window
+        self.connectSignals()
+
+    def buildingText(self, button_text):
+        display_text = self._window.getDisplayText()
+        text_to_include = display_text + button_text
+        self._window.setDisplayText(text_to_include)
+
+    def connectSignals(self):
+        for button_name, button_widget in self._window.buttons.items():
+            if not (button_name == "CE" or button_name == "="):
+                button_widget.clicked.connect(partial(self.buildingText, button_name))
+            if button_name == "CE":
+                button_widget.clicked.connect(self._window.clearDisplay)
 
 
 def main():
     app = QApplication(sys.argv)
     window = Calculator()
     window.show()
+    controller = Controller(window)
     sys.exit(app.exec_())
 
 
